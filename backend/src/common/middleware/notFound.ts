8cb20669632da263bb52e5b77851671fd81b62f0
Kaanyinaele <kaanyinaele@gmail.com>
2025-12-29 17:50:08 +0100

    removed backend folder

--- backend/src/common/middleware/notFound.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export function notFound(_req: Request, _res: Response, next: NextFunction) {
-  next({ status: 404, message: 'Not Found' });
-}
--- backend/src/modules/health/routes/health.route.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { health, kvDemo } from '../controllers/health.controller.js';
-const router = Router();
-router.get('/', health);
-router.get('/kv', kvDemo);
-export default router;
